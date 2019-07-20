723ae8c279f2e7d7794a670f80337048daa5ae85
KshitijSrivastava <kshitij127@yahoo.co.in>
2019-07-20 23:29:45 +0530

    Fully working REST API

--- RESTAPI/mozio/models.py
+++ RESTAPI/mozio/models.py
@@ -24,6 +24,13 @@ CURRENCY_CHOICES = [
 
 class Provider(models.Model):
     """
+    The Provider class defines the data storage for each provider
+    It has the following fields:
+    name -> Character field to store the name of the provider
+    email -> email field to store the email of the provider
+    phonenumber -> Big integer field to store the phone number
+    language -> Character field to choose the languages from LANGUAGE_CHOICES
+    currency -> Character field to choose the currency from CURRENCY_CHOICES
     """
     name = models.CharField(max_length = 100)
     email = models.EmailField()
@@ -33,3 +40,22 @@ class Provider(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class GeoPolygon(models.Model):
+    """
+    The GeoPolygon class defines the data storage for each polygon
+    It has the following fields:
+    provider -> It acts a foriegn key to Provider class allowing One to Many Relationships
+                So each Provider can have multiple GeoPolygon objects
+    price -> Decimal Field to allow price to be set for each polygon
+    poly -> Text Field to allow dimensions of the polygon to be set
+    """
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
+    polygon_name = models.CharField(max_length = 256, default = 'newpoly')
+    price = models.DecimalField(max_digits = 6 , decimal_places = 2, default = 0.00 )
+    poly = models.TextField()
+
+    def __str__(self):
+        return self.polygon_name
